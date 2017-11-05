d221390d05a53982e677971ad327359d953d1719
teddymisty132 <teddymisty132@gmail.com>
2017-11-04 22:56:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const prefix = "a.";
-
 client.on("ready", () => {
   console.log("I am ready!");
-  
 });
 
 client.on("message", (message) => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[1];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice[1];
-  
-  if (command === "say") {
-    message.channel.sendMessage(args.join(" "));
+  if (message.content === '*picme') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
   }
 });
 

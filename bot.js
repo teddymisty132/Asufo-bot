836e84fffb33658a466270cc41305a8d73913664
teddymisty132 <teddymisty132@gmail.com>
2017-11-04 23:18:43 -0400

    start
    Will add more from here

--- bot.js
+++ bot.js
@@ -18,21 +18,6 @@ client.on("message", (message) => {
   if (command === 'picme') {
     // Send the user's avatar URL
     message.reply(message.author.avatarURL);
-  } else {
-    if (command === "ban") {
-    if(message.mentions.users.size === 0) {
-      return message.reply("please mention a user to ban them from the guild.");
-    }
-    let kickMember = message.guild.member(message.mentions.users.first());
-    if (!kickMember) {}
-      return message.reply("this users does not seem valid.");
-    }
-    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) {
-      return message.reply("I do not have the permissions to ban members.");
-    }
-    kickMember.kick("You were banned because `" + message.reply(args.join(" ")) + "`");
-    message.author.send("You have successfuly banned " + kickMember.user + " from TKC Discord.")
-    }
   }
 });
 

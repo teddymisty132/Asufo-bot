504dc40a3b90cee3c44c779107dadd08aa7afbc7
teddymisty132 <teddymisty132@gmail.com>
2017-11-04 21:39:00 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const prefix = "a."
+
+client.on("ready", () => {
+  console.log("I am ready!");
+  
+});
+
+client.on("message", (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[1];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice[1];
+  
+  if (command === "say") {
+    message.channel.sendMessage(args.join(" "));
+  }
+});
+
+client.login(process.env.TOKEN);

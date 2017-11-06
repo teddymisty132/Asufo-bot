6f3b6c68780e21ac50007eac5751a41b68d7d045
teddymisty132 <teddymisty132@gmail.com>
2017-11-06 17:30:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("I am ready!");
-  client.user.setStatus('*help')
+  client.user.setGame('*help')
 });
 
 const prefix = "*"

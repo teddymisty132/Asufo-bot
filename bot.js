107dcc3c1fc1bde12dbcf27b2f94f0b2327553d3
teddymisty132 <teddymisty132@gmail.com>
2017-11-06 19:42:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,6 +58,20 @@ client.on("message", (message) => {
     }
   }
 
+  if (command === 'summon') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+
+
 });
 
 client.login(process.env.TOKEN);
